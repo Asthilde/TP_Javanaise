285728c282ce15687b812279b0e463189576fd02
Noémie_Pellegrin <noemie.pellegrin@etu.univ-grenoble-alpes.fr>
2024-10-20 22:23:31 +0200

    test(compteur): ajout du test burst compteur

--- JAVANAISE/src/Compteur/Compteur.java
+++ JAVANAISE/src/Compteur/Compteur.java
@@ -1,22 +1,27 @@
 package Compteur;
 
+import jvn.*;
 
-
-public class Compteur implements java.io.Serializable {
+public class Compteur implements CompteurInterface {
 	int comp;
 	
 	public Compteur () {
 		comp = 0;
 	}
 	
-	public void incrementCompteur() {
+	@Annotation(methodName="write")
+	public int incrementCompteur() {
 		comp += 1;
+		return comp;
 	}
 	
-	public void decrementCompteur() {
+	@Annotation(methodName="write")
+	public int decrementCompteur() {
 		comp -= 1;
+		return comp;
 	}
 	
+	@Annotation(methodName="read")
 	public int getCompteur() {
 		return comp;
 	}
